9bdfdc9f0f0dd9cd1c66d8718d45ad23cbc0020f
Shwetha <shwetha.d2021@kgkite.ac.in>
2023-07-17 21:33:45 +0530

    Metamask added

--- src/Main.js
+++ src/Main.js
@@ -5,21 +5,32 @@ import { Footer } from './components/Footer';
 import About from './components/About';
 
 
-
 const Main = () => {
+
+    const connectWallet = async () => {
+        if (window.ethereum) {
+          console.log('Detected');
+          try {
+            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+          } catch (error) {
+            console.log('Error connecting');
+          }
+        } else {
+          console.log('Not detected');
+        }
+    };
+    
+
     return (   
         <div className="main">
             <Navbar />
             <Carousel />
             <div className="content" id="content">
-               <button className="cn cn1">Become a Seller</button>
-               <button className="cn cn2">Start buying</button>
+               <button className="cn cn1" onClick={connectWallet}>Become a Seller</button>
+               <button className="cn cn2" onClick={connectWallet}>Start buying</button>
             </div>
             <About />
             <Footer />
-            
-            
-
         </div>     
 
   )
